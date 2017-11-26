ee4e667516b1a06aac04fb2da603197ae991989a
Tobias Schramm <tobleminer@gmail.com>
2017-11-26 03:01:09 +0100

    Convert logger to proper class

--- lib/logger.lua
+++ lib/logger.lua
@@ -1,27 +1,40 @@
-local logger = {}
+local util = require('lib/util.lua')
 
-logger.DEBUG = 0
-logger.INFO = 1
-logger.WARNING = 2
-logger.ERROR = 3
+local Logger = util.class()
 
-local function logger_log(self, msg, level)
+Logger.DEBUG = 0
+Logger.INFO = 1
+Logger.WARNING = 2
+Logger.ERROR = 3
+
+function Logger:log(msg, level)
 	local prefixed_msg = "[" .. self.name .. "] " .. msg
-	if level == logger.ERROR then
+	if level == Logger.ERROR then
 		error(prefixed_msg)
 	elseif level >= self.level then
 		print(prefixed_msg)
 	end
 end
 
-function logger.new(name, level)
-	return {	name = name,
-				level = level,
-				log = logger_log,
-				error = function(self, msg) self:log(msg, logger.ERROR) end,
-				warn = function(self, msg) self:log(msg, logger.WARNING) end,
-				info = function(self, msg) self:log(msg, logger.INFO) end,
-				debug = function(self, msg) self:log(msg, logger.DEBUG) end }
+function Logger:error(self, msg)
+	self:log(msg, Logger.ERROR)
 end
 
-return logger
+function Logger:warn(self, msg)
+	self:log(msg, Logger.WARNING)
+end
+
+function Logger:info(self, msg)
+	self:log(msg, Logger.INFO)
+end
+
+function Logger:debug(self, msg)
+	self:log(msg, Logger.DEBUG)
+end
+
+function Logger:init(name, level)
+	self.name = name
+	self.level = level
+end
+
+return Logger
